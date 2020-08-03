d306bae28cf78f99efdb1d04ebc2028acb59022b
arpitjain1009 <68836682+arpitjain1009@users.noreply.github.com>
2020-08-03 22:35:29 +0530

    Add files via upload

---
+++ 10670499_ArpitJain/10670499_ArpitJain/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import {ContactService} from './contact/contact.service';
+import {ReactiveFormsModule} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [ContactService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
